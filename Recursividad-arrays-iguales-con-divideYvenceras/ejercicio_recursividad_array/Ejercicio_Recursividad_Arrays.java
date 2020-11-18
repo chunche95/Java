80ca0acf93062f505a78878b051d03fc0c9cbf91
chunche95 <paeste95.pb@gmail.com>
2020-11-18 19:05:36 +0100

    Recursividad-Divide_y_vencerás

--- Recursividad-arrays-iguales-con-divideYvenceras/ejercicio_recursividad_array/Ejercicio_Recursividad_Arrays.java
+++ Recursividad-arrays-iguales-con-divideYvenceras/ejercicio_recursividad_array/Ejercicio_Recursividad_Arrays.java
@@ -1,5 +1,4 @@
 package ejercicio_recursividad_array;
-
 /**
  * Teniendo dos array, comprobamos con divide y venceras si los dos arrays son iguales.
  * 
@@ -9,7 +8,7 @@ package ejercicio_recursividad_array;
 public class Ejercicio_Recursividad_Arrays
 {
     public static void main (String[] args) {
-        int[] arr1 = { 1,2,3,4,5,6,7,9,9};
+        int[] arr1 = { 1,2,3,4,5,6};
         int[] arr2 = { 1,2,3,4,5,6,7,8,9};
         
         if(arr1.length == arr2.length){
@@ -20,8 +19,7 @@ public class Ejercicio_Recursividad_Arrays
             }
         }else{
             System.out.println("Los array no tienen la misma longitud");
-        }
-        
+        }        
         System.out.println("Diferencia en la posición: " + arraysIguales(0, arr1.length -1, arr1, arr2));
     }
     /* Ver en que posicion 'falla' o si todo está OK */
@@ -36,16 +34,14 @@ public class Ejercicio_Recursividad_Arrays
         }else{
             int mitad =  (inicio + fin)/2;
             int x = arraysIguales(inicio, mitad, arr1, arr2);
-            int y = arraysIguales(mitad +1 , fin , arr1, arr2);
-            
+            int y = arraysIguales(mitad +1 , fin , arr1, arr2);            
             if( x != -1){
                 /* Devuelve el 'fallo' donde no coincide el array */
                 return x;
             }
             if( y != -1 ){
                 return y;
-            }
-            
+            }            
             return -1;
         }
     }
