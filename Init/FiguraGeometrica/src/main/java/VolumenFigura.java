4f84533c53b1cb3903e8c70ce7af0d835ba26316
chunche95 <37621958+chunche95@users.noreply.github.com>
2020-12-08 22:56:18 +0100

    Ejercicio de volumen de un cubo

---
+++ Init/FiguraGeometrica/src/main/java/VolumenFigura.java
@@ -0,0 +1,21 @@
+/*
+ * Ejercicio.
+ * 
+ * Caja - Volumen = ancho x alto x profundidad
+ * 
+ * Debe tener:
+ * - 3 atributos de tipo entero (ancho, alto y largo)
+ * - 2 constructores, uno vacío y otro con 3 argumentos, uno por cada atributo de la clase
+ * - Método que calcule el volumen de la caja
+ * - Crear una clase de prueba para crear un objeto de tipo caja con los valores ancho = 3, alto = 2, largo = 6 e imprima el volumen
+ * - Probar modo debug y probar con otros datos.
+ * 
+ */
+
+/**
+ *
+ * @author pauli
+ */
+public class VolumenFigura {
+    
+}
