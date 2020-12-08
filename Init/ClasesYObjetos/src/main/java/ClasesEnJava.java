49ffe47dcc7b570f93e36c19006857821a257cc2
chunche95 <37621958+chunche95@users.noreply.github.com>
2020-12-08 19:20:16 +0100

    clases

---
+++ Init/ClasesYObjetos/src/main/java/ClasesEnJava.java
@@ -0,0 +1,24 @@
+/*
+ * Clases. Base de POO.
+ * Define la naturaleza de un objeto. Su definición 'sencilla' sería como una plantilla para definir más objetos.
+ * Objeto es una instancia de una clase
+ * # Tiene un nombre, posee atributos y métodos. #
+ * Característica de su estructura
+ * - Definimos atributos o variables de nuestra instancia.
+ * - Definimos los métodos de nuestras clases.
+ * 
+ * => métodos contienen el código y funcionalidad de la clase.
+ * 
+ * Tipos Object son: Clases, interfaces y arreglos.
+ */
+
+/**
+ *
+ * @author pauli
+ */
+public class ClasesEnJava {
+    public static void main(String[] args) {
+        
+    } // void main
+        
+} // clase ClasesEnJava
