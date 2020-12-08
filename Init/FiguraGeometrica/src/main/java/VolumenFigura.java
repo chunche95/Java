d9d3e1055e4fe4e338607d1543d79585e9b920c2
chunche95 <37621958+chunche95@users.noreply.github.com>
2020-12-09 00:41:46 +0100

    Creando caja por defecto y cajas posibles
    Cálculo de un volumen por defecto
    Cálculo de volumen para cualquier caja

--- Init/FiguraGeometrica/src/main/java/VolumenFigura.java
+++ Init/FiguraGeometrica/src/main/java/VolumenFigura.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Ejercicio.
  * 
@@ -11,18 +14,39 @@
  * - Probar modo debug y probar con otros datos.
  * 
  */
-
 /**
  *
  * @author pauli
  */
+import java.util.Scanner;
+
 public class VolumenFigura {
+
     public static void main(String[] args) {
+        
         /* Declaración de las variables de pruebas */
-        int ancho ,alto , largo ;
+        System.out.println("MEDIDAS DE LA CAJA. \n ____________________ ");
+        
+        System.out.println("Ancho: ");
+        Scanner an = new Scanner(System.in);
+        int ancho = an.nextInt();
+        
+        System.out.println("Alto: ");
+        Scanner al = new Scanner(System.in);
+        int alto = al.nextInt();
+        
+        System.out.println("Largo: ");
+        Scanner la = new Scanner(System.in);
+        int largo = la.nextInt();
+        
+        // ancho = 3, alto = 2, largo = 6
+        if (ancho != 0) {  ancho = ancho; } else { ancho = 3; }
+        if (alto != 0) { alto = alto; } else { alto = 2; }
+        if (largo != 0) { largo = largo; } else { largo = 6; }
         
         Caja nuevaCaja1 = new Caja(ancho, alto, largo);
-        System.out.println("Volumen de la caja es: "+ alto + " X "+ ancho + " X "+ largo + " = "  );
-        System.out.println(nuevaCaja1.volumen());
+        System.out.println("Caja Default => ancho: 3 | alto: 2 | largo: 6");
+        System.out.println("Volumen de la caja es: " + ancho + " X " + alto + " X " + largo + " = " + nuevaCaja1.volumen());
+
     }
 }
