87b97041a8ededf98997c75059d7002e8e0cff38
chunche95 <37621958+chunche95@users.noreply.github.com>
2020-12-08 21:04:04 +0100

    argumentos
    Tipos de argumentos en constructores y métodos

--- Init/ProyectoAritmetica/src/main/java/Aritmetica.java
+++ Init/ProyectoAritmetica/src/main/java/Aritmetica.java
@@ -7,11 +7,28 @@
  * @author pauli
  */
 public class Aritmetica {
+    
+    /*
+     * INICIALIZAMOS LOS ARGUMENTOS DE LA CLASE con sus valores por defecto.
+     * Sirven en caso de que el usuario NO introduzca ningún valor para estas variables, el programa 
+     * tomará los valores que definamos aquí, al no indicarle nada, será su V. default = 0.
+    */
+    int a; 
+    int b; 
+    
     /*
      * Constructor de la clase => Se llama igual que la clase
+     * Nos sirve para inicializar las variables de nuestra clase y reservar espacion en memoria.
     */
     public Aritmetica(){
-        System.out.println("Ejecutando constructor vacío");
+        a = 3; 
+        b = 10;
+        System.out.println("Ejecutando constructor vacío para reservar espacio en memoria.");
+    }
+    
+    public Aritmetica(int arg1 , int arg2){
+        a =  arg1;
+        b = arg2;
     }
     
     
@@ -20,10 +37,10 @@ public class Aritmetica {
      *      - public => Modificador de acceso
      *      - int => Tipo de regreso
      *      - sumar => Nombre del método
-     *      - (int a, int b) => Argumentos del método
+     *      - (int a, int b) => Argumentos del método, si los dejo vacios los paréntesis usará los valores por defecto.
      *  {} => Cuerpo del método
     */
-    public int sumar(int a, int b){
+    public int sumar(){
         int resultado = a+b;
         return resultado;
     }
--- Init/ProyectoAritmetica/src/main/java/PruebaAritmetica.java
+++ Init/ProyectoAritmetica/src/main/java/PruebaAritmetica.java
@@ -7,10 +7,18 @@
  * @author pauli
  */
 public class PruebaAritmetica {
+    
     public static void main(String[] args) {
         // Creamos un objeto de tipo aritmetica
         Aritmetica aritmetica = new Aritmetica();
-        int resultado = aritmetica.sumar(12, 50);
+        
+        // Inicializamos el constructor y asignamos los valores a los argumentos del método
+        int resultado = aritmetica.sumar();
         System.out.println("Resultado: " + resultado);
+        
+        // Creación de un segundo objeto  Aritmetica
+        Aritmetica aritmetica2 = new Aritmetica(5,6);
+        resultado = aritmetica2.sumar();
+        System.out.println("Resultado2 : " + resultado);
     }
 }
