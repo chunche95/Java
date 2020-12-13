324b8ab0347ce3cbf90ac569f9ec80cd9d8b4ba5
chunche95 <37621958+chunche95@users.noreply.github.com>
2020-12-13 19:14:31 +0100

    Ej. estático
    Retoque en ejemplo estático con palabras Ramdom como password y cambiando fallo en bucle for para la condicional del switch

--- Init/EjemploEstatico/src/main/java/EjEstatico.java
+++ Init/EjemploEstatico/src/main/java/EjEstatico.java
@@ -14,22 +14,32 @@ public class EjEstatico {
             
             switch(i){
                     case 1:
-                        Persona p = new Persona("Rico"+i);
+                        Persona p = new Persona("Marcos"+i);
                         System.out.println(p);
+                        break;
                         
                     case 2:
                         Persona p2 = new Persona("Luisa"+i);
                         System.out.println(p2);
+                        break;
                         
                     case 3:
                         Persona p3 = new Persona("Carlos"+i);
                         System.out.println(p3);
+                        break;
                         
                     case 4:
                         Persona p4 = new Persona("Paula"+i);
                         System.out.println(p4);
+                        break;
+                        
+                    case 5:
+                        Persona p5 = new Persona("Andrés"+i);
+                        System.out.println(p5);
+                        break;
+                        
                     default:
-                        System.out.println("Fallo de ejecución" + i);
+                        System.out.println("No hay usuario - " + i);
                         
             }
             
--- Init/EjemploEstatico/src/main/java/Persona.java
+++ Init/EjemploEstatico/src/main/java/Persona.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -40,9 +43,22 @@ public class Persona {
         return contadorPersonas;
     }
     
+     String generateRandomWord(int wordLenth){
+        Random r = new Random(); // Intialize a Random Number Generator with SysTime as the seed
+        int wordLength = 10;
+       StringBuilder sb = new StringBuilder(wordLength);
+       for(int i = 0; i < wordLength; i++) {  // For each letter in the word
+            char tmp = (char) ('a' + r.nextInt('z' - 'a'));  // Generate a letter between a and z
+            sb.append(tmp); // Add it to the String
+       }
+       return sb.toString();
+     }
+    
     @Override
     public String toString(){
-        return "ID Persona: " + idPersona +  "\n\t Nombre: " + nombre + "\n\t\t contador: " + contadorPersonas;
+        return   "........................ contador: " + contadorPersonas + "........................" +" \nNombre: " + nombre +"\n\t ID Persona: " + idPersona +   "\n\t Password: " + nombre+"-"+generateRandomWord(contadorPersonas);
     }
+    
+   
     
 }
