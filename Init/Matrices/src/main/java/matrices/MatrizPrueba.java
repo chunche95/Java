3a2ad328260435440eba2b82e6d923bf8680c580
chunche95 <37621958+chunche95@users.noreply.github.com>
2020-12-16 11:27:18 +0100

    Matrices con bucle for anidado

--- Init/Matrices/src/main/java/matrices/MatrizPrueba.java
+++ Init/Matrices/src/main/java/matrices/MatrizPrueba.java
@@ -20,13 +20,13 @@ public class MatrizPrueba {
                                             COLUMNAS
                                    ________/\ ______
                                 /                               \
-                                 +___ ___ ___  _____+
-                                 |_0.0_|_0.1_|_0.2_|
-                                 |_1.0_|_1.1_|_1.2_|
-                    FILAS -->|_2.0_|_2.1_|_2.2_|
-                                 |_3.0_|_3.1_|_3.2_|
-                                 |_4.0_|_4.1_|_4.2_|
-                                 +_5.0_|_5.1_|_5.2 +
+                              /  +___ ___ ___  _____+
+                             |   |_0.0_|_0.1_|_0.2_|
+                             |   |_1.0_|_1.1_|_1.2_|
+                   FILAS <    |_2.0_|_2.1_|_2.2_|
+                             |   |_3.0_|_3.1_|_3.2_|
+                             |   |_4.0_|_4.1_|_4.2_|
+                              \  +_5.0_|_5.1_|_5.2 +
         */
         edades[0][0]= 35;
         edades[0][1]= 30;
@@ -39,7 +39,38 @@ public class MatrizPrueba {
         System.out.println("Matriz enteros: índice 0.0: " + edades[0][0]);
         System.out.println("Matriz enteros: índice 0.1: " + edades[0][1]);
         System.out.println("Matriz enteros: índice 1.1: " + edades[1][1]);
-        System.out.println("Matriz enteros: índice 2.0: " + edades[2][0]);
+        System.out.println("Matriz enteros: índice 2.0: " + edades[2][0]);        
+        System.out.println("..........................................");
+        
+        
+        // Declaracion e instancia de la matriz de personas de la clase Persona
+        Persona personas[][] = new Persona[2][3];
+        // Iniciañización y asignación de valores
+        personas[0][0] = new Persona("Pilar");
+        personas[0][1] = new Persona("Paco");
+        personas[0][2] = new Persona("Paco");
+        
+        personas[1][0] = new Persona("Patricia");
+        personas[1][1] = new Persona("Pablo");
+        
+        // Imprimimos valores
+        System.out.println("..........................................");
+        System.out.println("Valores de matriz");
+        System.out.println("0.0" + personas[0][0]);
+        System.out.println("0.1" + personas[0][1]);
+        System.out.println("1.0" + personas[1][0]);
+        System.out.println("1.1" + personas[1][1]);
+        System.out.println("..........................................");
+        
+        for (int fila = 0; fila < personas.length; fila++){
+            for(int columna = 0; columna < personas[fila].length; columna++){
+                System.out.println("..........................................");
+                System.out.println("For fila: " + fila +" columna: " + columna);
+                System.out.println("Valor persona - " +  fila + ":" + columna +"=" + personas[fila][columna]);
+            }
+        }
+        
+        
         
     }
 }
